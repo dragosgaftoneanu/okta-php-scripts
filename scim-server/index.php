6d0c22c4f4fd00f2af1480326d8372922360794e
Dragos Gaftoneanu <43792974+dragosgaftoneanu@users.noreply.github.com>
2020-10-02 18:22:28 +0300

    Update index.php

--- scim-server/index.php
+++ scim-server/index.php
@@ -1,11 +1,17 @@
 <?php
-/**
- * scimify
- * Author: Dragos Gaftoneanu <dragos.gaftoneanu@okta.com>
+/** Copyright © 2018-2019, Okta, Inc.
  *
- * Disclaimer: This SCIM server was built in order to simulate and troubleshoot different SCIM use-cases and not to be used in production. The script is provided AS IS
- * without warranty of any kind. Okta disclaims all implied warranties including, without limitation, any implied warranties of fitness for a particular purpose. We highly
- * recommend testing scripts in a preview environment if possible.
+ *  Licensed under the Apache License, Version 2.0 (the 'License');
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 /* Hide any warning or errors, remove this fields if you need to debug */
